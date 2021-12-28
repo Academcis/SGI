a0969ca7108193a689ed51ac3e6e01f55a161f28
andrepintoo <88459198+andrepintoo@users.noreply.github.com>
2021-12-28 20:13:29 +0000

    final

--- Projeto/main_movel.js
+++ Projeto/main_movel.js
@@ -1,10 +1,8 @@
 var scene = new THREE.Scene()
 
-var camera = new THREE.PerspectiveCamera(70, 800/600,0.01, 1000)
-camera.position.set(6,4,7) //em vez de fazer camera.position.z=3 para x, y e z 
-camera.lookAt(0,0,0) //para onde quero apontar a camera (neste caso, para o centro)
-
-//document.body.appendChild(renderer.domElement)
+var camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight,0.01, 1000)
+camera.position.set(5,4,9) 
+camera.lookAt(0,7,-2)
 
 var clock = new THREE.Clock()
 var mixer = new THREE.AnimationMixer(scene)
@@ -12,12 +10,12 @@ var mixer = new THREE.AnimationMixer(scene)
 var actionLeftDoorAction = null
 var actionRigthDoorAction = null
 
-var myCanvas = document.getElementById("myCanvas") //- não é preciso pois já tenho o appendChild do body
+var myCanvas = document.getElementById("myCanvas")
 
-//var grid = new THREE.GridHelper()
-//scene.add(grid)
-//var axes = new THREE.AxesHelper(5) //tamanho dos eixos
-//scene.add(axes)
+var grid = new THREE.GridHelper()
+scene.add(grid)
+var axes = new THREE.AxesHelper(8)
+scene.add(axes)
 
 var renderer = new THREE.WebGLRenderer({canvas:myCanvas})
 renderer.setSize(545,400)
@@ -25,28 +23,47 @@ renderer.shadowMap.enabled = true
 
 new THREE.OrbitControls(camera, renderer.domElement)
 
+var target = null
+var doors = []
+var raycaster = new THREE.Raycaster()
+var mouse = new THREE.Vector2()
+
 loadScene()
 animate()
 addlights()
 actionButtons()
 
-
 function loadScene(){
     var loadBench = new THREE.GLTFLoader()
     loadBench.load( 
-        'models/movelJardinagem_v3.gltf',
+        'models/movelJardinagem_v2.gltf',
         function(gltf){
             scene.add(gltf.scene)
             gltf.scene.traverse(function(x){
                 if (x instanceof THREE.Light) x.visible = false
-                //scene.getObjectByName('Botao2').visible = false -> depois de ver na consola qual é o nome 
-                //scene.getObjectByName('door').visible = true
-            }) //para tornar invisivel a luz que possa existir a mais
-
-            var leftDoorAction = THREE.AnimationClip.findByName(gltf.animations, "NlaLeftDoorAction") 
-            var rightDoorAction = THREE.AnimationClip.findByName(gltf.animations, "NlaRigthDoorAction") 
+            })
+            //scene.getObjectByName("rightDoor").visible = true
+            var leftDoorAction = THREE.AnimationClip.findByName(gltf.animations, "NlaLD") 
+            var rightDoorAction = THREE.AnimationClip.findByName(gltf.animations, "NlaRD") 
             actionLeftDoorAction = mixer.clipAction(leftDoorAction)
             actionRigthDoorAction = mixer.clipAction(rightDoorAction)
+
+            scene.traverse(function(objMesh){
+                if(objMesh.isMesh){
+                    objMesh.castShadow = true
+                    objMesh.receiveShadow = true
+                }
+
+                if(objMesh.name == "rightDoor"){ 
+                    doors[0] = objMesh //só entra aqui se encontrar o "rightDoor"
+                }else if(objMesh.name == "leftDoor"){
+                    doors[1] = objMesh //só entra aqui se encontrar o "leftDoor"
+                }  
+            })
+
+            for(var i = 0; i < doors.length; i++){
+                console.log("Mesh added to doors []: " + doors[i].name)
+            }
         }
     )
 }
@@ -59,77 +76,140 @@ function animate(){
 
 
 function addlights(){
-    var ambientLight = new THREE.AmbientLight("white", 0.45) //nunca mostra sombras, ilumina para todos os lados de igual forma
+    var ambientLight = new THREE.AmbientLight("white", 0.55)
     scene.add(ambientLight)
 
-    //Sem ser o ambientLigth, se existisse um objeto que tapa a luz a partir do ponto onde coloco-a, ficava preto
     var pointLight = new THREE.PointLight("white")
     pointLight.position.set(10,6,0)
     pointLight.castShadow = true
     scene.add(pointLight)
 }
 
+// var hemilight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+// hemiLight.color.setHSV( 0.6, 0.75, 0.5 );
+// hemiLight.groundColor.setHSV( 0.095, 0.5, 0.5 );
+// hemiLight.position.set( 0, 500, 0 );
+// scene.add(hemilight);
+
+var esq_aberta = 0
+var dir_aberta = 0
+var pausa = 0
+var dia = 0
 
  function actionButtons(){
-     document.getElementById("btn_left_door_open").onclick = function (){
-         //actionBenchExtendDown.play()
-         actionLeftDoorAction.play()
-         actionLeftDoorAction.setLoop(THREE.LoopOnce)
-         actionLeftDoorAction.clampWhenFinished = true //por default isto está a false
+     document.getElementById("btn_left_door_open").onclick = function(){
+        if(esq_aberta == 0){
+            camera.position.set(2,3,5)
+            camera.lookAt(0,0,2)
+            actionLeftDoorAction.reset()
+            actionLeftDoorAction.timeScale = 1
+            actionLeftDoorAction.setLoop(THREE.LoopOnce)
+            actionLeftDoorAction.clampWhenFinished = true
+            actionLeftDoorAction.play()
+            esq_aberta = 1
+        }
+        
      }
 
      document.getElementById("btn_left_door_close").onclick = function(){
-        // actionBenchExtendDown.paused = !actionBenchExtendDown.paused  //fica no estado contrário do que estava
-       // actionLeftDoorAction.play() = -actionLeftDoorAction.play()
-        actionLeftDoorAction.timeScale = -actionLeftDoorAction.timeScale
-        actionLeftDoorAction.setLoop(THREE.LoopOnce)
-        actionLeftDoorAction.clampWhenFinished = true
-     }
-
-     document.getElementById("btn_right_door_open").onclick = function () {
-         actionRigthDoorAction.play()
-         actionRigthDoorAction.setLoop(THREE.LoopOnce)
-         actionRigthDoorAction.clampWhenFinished = true
-     }
-
-     document.getElementById("btn_right_door_close").onclick = function () {
-         actionRigthDoorAction.timeScale = -actionLeftDoorAction.timeScale
-         actionRigthDoorAction.setLoop(THREE.LoopOnce)
-         actionRigthDoorAction.clampWhenFinished = true
-     }
-
-    document.getElementById("btn_stop").onclick = function(){
-        //actionBenchExtendDown.stop()
-        actionLeftDoorAction.stop()
-    }
-    document.getElementById("btn_pause").onclick = function(){
-        actionLeftDoorAction.paused = !actionLeftDoorAction.paused
-        actionRigthDoorAction.paused = !actionRigthDoorAction.paused
-    }
-
-    document.getElementById("btn_reverse").onclick = function(){
-        actionLeftDoorAction.timeScale = -actionLeftDoorAction.timeScale
-        
-    }
-
-    document.getElementById("menu_loop").onclick = function(){
-        switch(this.value){ 
-            case "1"://once
-                //actionBenchExtendDown.setLoop(THREE.LoopOnce)
-                //actionBenchExtendDown.clampWhenFinished = true //para quando se faz play, para ele ficar no frame onde acaba (para nao fazer reset)
-                actionLeftDoorAction.setLoop(THREE.LoopOnce)
-                actionLeftDoorAction.clampWhenFinished = true //por default isto está a false
-                break;
-        
-            case "2"://loop
-                //actionBenchExtendDown.setLoop(THREE.LoopRepeat)
-                actionLeftDoorAction.setLoop(THREE.LoopRepeat)
-                break;
-        
-            case "3"://boomerang
-                //actionBenchExtendDown.setLoop(THREE.LoopPingPong)
-                actionLeftDoorAction.setLoop(THREE.LoopPingPong)
-                break;
+        if(esq_aberta == 1){
+            actionLeftDoorAction.timeScale = -1  
+            actionLeftDoorAction.setLoop(THREE.LoopOnce)   
+            actionLeftDoorAction.clampWhenFinished = true
+            actionLeftDoorAction.paused = false  
+            actionLeftDoorAction.play() 
+            esq_aberta = 0
         }
+     }
+
+     document.getElementById("btn_right_door_open").onclick = function() {
+        if(dir_aberta == 0){
+            camera.position.set(2,3,5)
+            camera.lookAt(0,0,2)
+            actionRigthDoorAction.reset()
+            actionRigthDoorAction.timeScale = 1
+            actionRigthDoorAction.setLoop(THREE.LoopOnce)
+            actionRigthDoorAction.clampWhenFinished = true
+            actionRigthDoorAction.play()
+            dir_aberta = 1
+        }
+     }
+
+     document.getElementById("btn_right_door_close").onclick = function() {
+        if(dir_aberta == 1){
+            actionRigthDoorAction.timeScale = -1  
+            actionRigthDoorAction.setLoop(THREE.LoopOnce)   
+            actionRigthDoorAction.clampWhenFinished = true
+            actionRigthDoorAction.paused = false  
+            actionRigthDoorAction.play() 
+            dir_aberta = 0
+        }
+    }
+
+     document.getElementById("btn_stop").onclick = function(){
+         actionLeftDoorAction.stop()
+         actionRigthDoorAction.stop()
+         dir_aberta = 0
+         esq_aberta = 0
+     }
+     
+     document.getElementById("btn_pause").onclick = function(){
+         if(pausa == 0){
+            actionLeftDoorAction.paused = true
+            actionRigthDoorAction.paused = true
+            pausa = 1
+        }else{
+            actionLeftDoorAction.paused = false
+            actionRigthDoorAction.paused = false
+            pausa = 0
+        }
+    }
+
+    document.getElementById("btn_reset_view").onclick = function(){
+        camera.position.set(5,4,9) 
+        camera.lookAt(0,7,-2)  
+    }
+
+    document.getElementById("btn_day_night").onclick = function(){
+        if(dia == 0){
+            dia = 1
+        }else{
+            dia = 0
+        }
+        if(dia == 0){ //dia
+            light = new THREE.SpotLight(0xffa95c,7);
+            light.position.set(-50,50,50);
+            light.castShadow = true;
+        }
+        else{ //noite
+
+        }
+        scene.add(light);
+        renderer.render(scene,camera);
+    }
+}
+window.onclick = function(event){
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1 
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1  
+    console.log("x: "+ mouse.x + "y: "+ mouse.y)
+    pickDoor()
+}
+
+// window.addEventListener('click', event => {
+//     raycaster.setFromCamera(mouse, camera)
+//     const found = raycaster.intersectObjects(scene.children)
+//     if(found.length > 0 && (found[0].object.getObjectByName() == doors[0] || found[0].object.getObjectByName() == doors[1])) {
+//         console.log('Found -> ' + found[0].object.getObjectByName())
+//     }
+//     if(found.length > 0){
+//         console.log(found[0].object.getObjectByName())
+//     }
+// })
+
+function pickDoor(){
+    raycaster.setFromCamera(mouse,camera)
+    var intersectedObjects = raycaster.intersectObjects(doors)
+    if(intersectedObjects.length > 0){
+        console.log('I got a door'+ intersectedObjects[0].object)
     }
 }
